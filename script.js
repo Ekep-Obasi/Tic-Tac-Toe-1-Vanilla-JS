d0f8ef0bd8839227e0bf975fe9f4950d4db62348
Ekep Obasi <arreyettaekep@gmail.com>
2023-03-22 01:59:57 +0100

    Built the winner's result popup

--- script.js
+++ script.js
@@ -30,16 +30,19 @@ function updateBoard(idx) {
 }
 
 function getRandomPosition() {
-  console.log("random position");
   const randomPosition = Math.floor(Math.random() * 9);
   const [row, col] = getPosition(randomPosition);
   console.log(row, col);
   while (board[row][col] !== null) {
     getRandomPosition();
     const stoppingCondition = board
-      .map((row) => row.filter((el) => el !== null).reduce((a, b) => a + b, 0))
+      .map((row) =>
+        row
+          .map((el) => (el !== null ? (el = 1) : (el = null)))
+          .reduce((a, b) => a + b, 0)
+      )
       .reduce((a, b) => a + b, 0);
-    if (stoppingCondition.length === 9) break;
+    if (stoppingCondition === 9) break;
     console.log(` stop : ${stoppingCondition}`);
   }
   markRandomPosition(row, col);
@@ -52,4 +55,16 @@ function markRandomPosition(row, column) {
     squareElement[position].classList.add("cross");
   }, 500);
   console.log(board);
+}
+
+const quitElement = document.querySelector(".quit");
+
+quitElement.addEventListener("click", quitGame);
+
+function quitGame() {
+  console.log("clicked");
+  const winState = document.querySelector(".winstate");
+  const winStateMsg = document.getElementById("msg");
+  winState.classList.add("fullscreen");
+  winStateMsg.classList.add("fullscreen");
 }
